322b8e8250b1cfa189d123bb121a90f056517635
Cesar <perezvaldesruben@gmail.com>
2023-01-02 16:33:14 -0600

    enhanced language selection, added new background images, enhanced JS

--- js/lib.js
+++ js/lib.js
@@ -1,4 +1,5 @@
-function change_language(show,hide) {
+// this funtion works with the buttons ESP / ENG
+function change_language(show,hide) { 
     var to_show = Array.from(document.getElementsByClassName(show));
     to_show.forEach(element => {
         element.style.display= 'block';
@@ -9,19 +10,29 @@ function change_language(show,hide) {
         element.style.display = 'none';
         });  
     
+    active_button = document.getElementById('to_' + show);
+    active_button.style.opacity = "1"
+
+    //will change to array after introducing french 
+    inactive_button = document.getElementById('to_' + hide); 
+    inactive_button.style.opacity = "0.6"
+
     var selected_lan = show;
     var hide_lan = hide;
     localStorage.setItem("selected_lan", selected_lan);
     localStorage.setItem("hide_lan", hide_lan);
+    localStorage.setItem("active_button_lan", 'to_'+show);
     }
-
+//This funtion works when the page is loaded and relies on the last button selection (funtion change_language())
 function load_language(){
     if(localStorage.getItem("selected_lan") == undefined){
         var selected_lan = 'eng';
         var hide_lan = 'esp';
+        var active_button_lan = 'to_eng';
     } else{
         var selected_lan = localStorage.getItem("selected_lan");
         var hide_lan = localStorage.getItem("hide_lan");
+        var active_button_lan = localStorage.getItem("active_button_lan");
     }
     
     var to_show = Array.from(document.getElementsByClassName(selected_lan));
@@ -33,25 +44,36 @@ function load_language(){
     to_hide.forEach(element => {
         element.style.display = 'none';
         });  
+        
+    active_button = document.getElementById(active_button_lan);
+    active_button.style.opacity = "1"
 }
-
+//This changes the banner backgroud images, changes the color used on side navigation, table headers and titles
 function update_theme(){
     var now = new Date();
     var current = now.getHours();
     switch(true){
-        case (current >= 0 && current < 6):
+        case (current >= 0 && current < 4):
             img = 'img/Montreal.jpg'; 
             color = 'rgb(48, 68, 236)';
             break;
-        case (current >= 6 && current < 12):
+        case (current >= 4 && current < 8):
             img = 'img/real_monte1.jpg';
             color = 'rgb(156, 184, 203)';
             break;
-        case (current >= 12 && current < 18):
+        case (current >= 8 && current < 12):
+            img = 'img/real_monte2.jpg';
+            color = 'rgb(156, 184, 203)';
+            break;
+        case (current >= 12 && current < 16):
             img = 'img/Seoul.jpg';
             color = 'rgb(95, 124, 255)';  
             break;
-        case (current >= 18 && current < 24):
+        case (current >= 16 && current < 20):
+            img = 'img/real_monte4.jpeg';
+            color = 'rgb(147, 17, 34)';
+            break;
+        case (current >= 20 && current < 24):
             img = 'img/view_home1.jpg';
             color = 'rgb(33, 54, 22 )';
             break;
