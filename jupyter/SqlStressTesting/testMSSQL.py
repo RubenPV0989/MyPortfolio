b4cdecf2d1c7f43c5551943071c81623620d0982
Cesar <perezvaldesruben@gmail.com>
2023-02-05 09:27:56 -0600

    adding sql stress artitle, updating filescan content

---
+++ jupyter/SqlStressTesting/testMSSQL.py
@@ -0,0 +1,44 @@
+import pyodbc
+import pandas as pd
+import datetime
+import os
+
+
+report_path = 'D:\\My Projects\\R\\SQLiteStressTest\\report_MSSQL.csv'
+
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};Server=LAPTOP-UJOFAFFN\SQLEXPRESS;Database=master;Trusted_Connection=yes')
+cursor = cnxn.cursor()
+
+for x in range(1,60): #insert columns 
+    columns_def = str(['field_'+str(x) + ' varchar(10)' for x in range(0,x)]).lstrip('[').rstrip(']').replace("'", "")
+    columns = str(['field_'+str(x) for x in range(0,x)]).lstrip('[').rstrip(']').replace("'", "")
+    values_tuple = tuple('field_'+str(x) for x in range(0,x))
+
+    create_table_query = 'CREATE TABLE python_test.dbo.main({});'.format(columns_def)
+    cursor.execute(create_table_query)
+    for y in range(1,50): #insert rows 
+        ops = list(['INSERT', 'SELECT'])
+        for op in ops: #switch between INSERT / SELECT
+            if op == 'INSERT':  
+                params = [values_tuple for x in range (y*100)]
+                param_num = str('?,' * len(values_tuple)).rstrip(',')
+                query = f'INSERT INTO python_test.dbo.main ({columns}) VALUES ({param_num});'
+                start_op = datetime.datetime.now()
+                cursor.executemany(query, params)
+                finish_op = datetime.datetime.now()
+                
+            else:
+                query = 'SELECT * FROM python_test.dbo.main'
+                start_op = datetime.datetime.now()
+                cursor.execute(query)
+                finish_op = datetime.datetime.now()
+
+            op_diff = (finish_op - start_op).total_seconds()
+            rows = cursor.execute('SELECT COUNT(*) FROM python_test.dbo.main').fetchone()[0]
+            evaluation = pd.DataFrame({'cols':[x], 'rows' : [rows], 'inserted_rows':[y*100], 'start_op' : [start_op], 'end_op' : [finish_op], 'time_diff': [op_diff], 'operation':[op]})
+            if not os.path.exists(report_path):
+                evaluation.to_csv(report_path, mode='a', index=False, header=True)
+            else:
+                evaluation.to_csv(report_path, mode='a', index=False, header=False)      
+    cursor.execute('DROP TABLE python_test.dbo.main')
+cursor.close()
---
+++ jupyter/SqlStressTesting/testSQLite.py
@@ -0,0 +1,34 @@
+import sqlite3
+import pandas as pd
+import datetime
+import os
+
+report_path = 'D:\\My Projects\\R\\SQLiteStressTest\\report_SQLite.csv'
+db_path = 'D:\\My Projects\\R\\SQLiteStressTest\\master.sqlite'
+conn = sqlite3.connect(db_path)
+cur = conn.cursor()
+for x in range(1,60): #insert columns
+    columns = str(['field_'+str(x) for x in range(0,x)]).lstrip('[').rstrip(']')
+    create_table_query = f'CREATE TABLE main({columns})'
+    cur.execute(create_table_query)
+    for y in range(1,50): #insert rows
+        values = columns.replace("'",'"')
+        ops = list(['INSERT', 'SELECT'])
+        for op in ops: #switch between INSERT / SELECT
+            if op == 'INSERT':                
+                query = f'INSERT INTO main ({columns}) VALUES ({values});'*(y*100)
+            else:
+                query = 'SELECT * FROM main'
+            start_op = datetime.datetime.now()
+            cur.executescript(query)
+            finish_op = datetime.datetime.now()
+            op_diff = (finish_op - start_op).total_seconds()
+            rows = cur.execute('SELECT COUNT(*) FROM main').fetchone()[0]
+            file_stats = os.stat(db_path)
+            evaluation = pd.DataFrame({'cols':[x], 'rows' : [rows], 'inserted_rows':[y*100], 'start_op' : [start_op], 'end_op' : [finish_op], 'time_diff': [op_diff], 'operation':[op], 'file_size(KB)': file_stats.st_size/1024})
+            if not os.path.exists(report_path):
+                evaluation.to_csv(report_path, mode='a', index=False, header=True)
+            else:
+                evaluation.to_csv(report_path, mode='a', index=False, header=False)      
+    cur.execute('DROP TABLE main')
+conn.close()
